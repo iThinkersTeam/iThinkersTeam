a8eeb1d88031f743bbc3cf3b408a26730ae8003b
User <user@Users-MacBook-Air.local>
2017-04-05 11:21:40 +0300

    new

---
+++ Examples/DesignableButton/DesignableButton/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  DesignableButton
+//
+//  Created by Ivan Akulov on 06/02/2017.
+//  Copyright © 2017 Andrii Bala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
